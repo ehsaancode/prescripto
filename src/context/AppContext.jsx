4025868f82fdaba1003f83468719ef7ea42bb77d
ehsaan <mail.ehsaan@gmail.com>
2025-07-07 21:20:16 +0530

    add context

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createContext } from "react";
+export const AppContext = createContext()
+
+import {doctors} from '../assets/assets';
+
+const AppContextProvider = (props)=>{
+
+    const value = {
+        doctors
+    }
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
