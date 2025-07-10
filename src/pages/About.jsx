29ef8b23a809054452e2132bbf05d912150458ca
ehsaan <mail.ehsaan@gmail.com>
2025-07-10 17:31:08 +0530

    add contact and more

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi facilis ipsa nihil asperiores, sit nulla dolorem debitis quos consectetur unde?
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quasi tenetur a delectus dignissimos harum perspiciatis blanditiis. Saepe eos ipsa tenetur asperiores repellendus quae pariatur!</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem dolorem natus, pariatur sit officia sint.</p>
+        </div>
+      </div>
+
+      <div>
+        <p className='text-xl my-4'>WHY <span className='text-gray-700 font-bold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>EFFICIENCY:</b>
+          <p>Stremlined appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>CONVINIENCE:</b>
+          <p>Access to a network of trusted healthcare professionals in your area.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>PERSONALIZATION:</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
+        </div>
+      </div>
     </div>
   )
 }
